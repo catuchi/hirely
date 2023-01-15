99e3e4753e232c2828843b8cc2a343c7be8805a9
Chibu <atuchibueze@gmail.com>
2023-01-15 17:53:44 -0500

    create Job model

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
+const dotenv = require("dotenv").config();
+const colors = require("colors");
 require("express-async-errors");
 
 // extra security features
@@ -7,3 +7,13 @@ const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
